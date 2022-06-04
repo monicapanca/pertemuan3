3f5833f3a292dde670bb5a52f2489f57972ae53e
monicapanca <pancaindrani@gmail.com>
2022-06-04 13:13:40 +0700

    initial commit add  sesi 1

---
+++ contoh3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  4 10:18:29 2022
+
+@author: monic
+"""
+
+print(4.2)
+print(type(4.2))
+
+print(4.)
+
+print(.2)
+
+print(.4e7)
+
+print(4.2e-4)
---
+++ contoh5.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  4 10:20:10 2022
+
+@author: amor
+"""
+
+print(type(True))
+
+print(type(False))
